f38366f02fee7b4d586952d3f8d8b195adfdf53b
Mikhail Avramenko <avramenkomy@yandex.ru>
2021-04-06 11:28:20 +0300

    modified README file

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')#.replace("://", "ql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = 'LongAndRandomSecretKey'
